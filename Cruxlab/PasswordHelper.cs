689271dfe388da738fc4e65870b06d0410370fe5
Vitaliy <vitalik.vinnik.05@gmail.com>
2023-02-17 14:20:08 +0200

    initial commit

---
+++ Cruxlab/PasswordHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Cruxlab;
+
+public static class PasswordHelper
+{
+    private static readonly Regex _rowFormat= new(@"^[^\s]\s\d+-\d+:\s[^\s]+$");
+    
+    public static bool ContainsSymbol(
+        string password,
+        char symbol,
+        uint minMatch,
+        uint maxMatch)
+    {
+        if (string.IsNullOrEmpty(password))
+        {
+            throw new ArgumentNullException(password, "Password is Empty");
+        }
+
+        var count = password.Count(x => x == symbol);
+        return count >= minMatch && count <= maxMatch;
+    }
+
+    public static (string password, char symbol, uint minMatch, uint maxMatch) ParseRow(string row)
+    {
+        if (string.IsNullOrEmpty(row))
+        {
+            throw new ArgumentNullException(row, "Row is Empty");
+        }
+
+        if (!_rowFormat.IsMatch(row))
+        {
+            throw new InvalidOperationException("Row format is Incorrect");
+        }
+
+        var data = row.Split(' ');
+        var symbol = char.Parse(data[0]);
+        var range = data[1]
+            .Trim(':')
+            .Split('-');
+        var minMatch = uint.Parse(range[0]);
+        var maxMatch = uint.Parse(range[1]);
+        var password = data[2];
+
+        return (password, symbol, minMatch, maxMatch);
+    }
+}
---
+++ Cruxlab/Program.cs
@@ -0,0 +1,31 @@
+﻿using Cruxlab;
+
+var filePath = Path.Combine(Directory.GetCurrentDirectory(), "passwords.txt");
+
+if (File.Exists(filePath))
+{
+    var count = 0;
+    var data = File.ReadAllLines(filePath);
+    foreach (var row in data)
+    {
+        try
+        {
+            var res = PasswordHelper.ParseRow(row);
+            if (PasswordHelper.ContainsSymbol(res.password, res.symbol, res.minMatch, res.maxMatch))
+            {
+                count++;
+            }
+        }
+        catch (InvalidOperationException)
+        {
+            Console.WriteLine($"row - {row} is in wrong format");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    Console.WriteLine($"Result: {count}");
+}
+
